859c89c787b2dda060423c6c0578e0e5c8269efe
carl <cb_1974@protonmail.com>
2024-10-04 19:13:01 +0200

    add scroll when no positions found

--- src/app/portfolio/no-positions-found.tsx
+++ src/app/portfolio/no-positions-found.tsx
@@ -1,6 +1,7 @@
 import { css } from "@/styled-system/css"
 import { VStack } from "@/styled-system/jsx"
 import React from "react"
+import ConnectWallet from "../components/navigation/ConnectWallet"
 
 const NoPositionsFound = () => {
   return (
@@ -11,92 +12,96 @@ const NoPositionsFound = () => {
         backgroundSize: "cover",
         backgroundPosition: "center",
         height: "70vh",
-        padding: "2rem"
+        padding: "2rem",
+        overflow: "scroll"
       })}
     >
-      <span
-        className={css({
-          fontSize: ["lg", "x-large"],
-          fontWeight: "bold",
-          marginBottom: "1rem",
-          display: "block",
-          color: "#fff"
-        })}
-      >
-        It looks like you don&apos;t have any active DCA (Dollar Cost Averaging)
-        positions yet. You are missing out on:
-      </span>
-      <ul>
-        <li
+      <div className={css({ overflow: "scroll" })}>
+        <span
           className={css({
-            fontSize: ["md", "lg"],
-            marginBottom: "0.5rem",
-            color: "#fff" // Optional: Adjust text color for contrast
+            fontSize: ["lg", "x-large"],
+            fontWeight: "bold",
+            marginBottom: "1rem",
+            display: "block",
+            color: "#fff"
           })}
         >
-          <span
+          It looks like you don&apos;t have any active DCA (Dollar Cost
+          Averaging) positions yet. You are missing out on:
+        </span>
+        <ul>
+          <li
             className={css({
-              fontWeight: "bold"
+              fontSize: ["md", "lg"],
+              marginBottom: "0.5rem",
+              color: "#fff" // Optional: Adjust text color for contrast
             })}
           >
-            - Low Fees:
-          </span>{" "}
-          You only cover transaction costs, keeping your expenses minimal.
-        </li>
-        <li
-          className={css({
-            fontSize: ["md", "lg"],
-            marginBottom: "0.5rem",
-            color: "#fff" // Optional: Adjust text color for contrast
-          })}
-        >
-          <span
+            <span
+              className={css({
+                fontWeight: "bold"
+              })}
+            >
+              - Low Fees:
+            </span>{" "}
+            You only cover transaction costs, keeping your expenses minimal.
+          </li>
+          <li
             className={css({
-              fontWeight: "bold"
+              fontSize: ["md", "lg"],
+              marginBottom: "0.5rem",
+              color: "#fff" // Optional: Adjust text color for contrast
             })}
           >
-            - Optimized Execution:
-          </span>{" "}
-          Breaks large swaps into smaller ones, helping you achieve better
-          overall pricing.
-        </li>
-        <li
-          className={css({
-            fontSize: ["md", "lg"],
-            marginBottom: "0.5rem",
-            color: "#fff" // Optional: Adjust text color for contrast
-          })}
-        >
-          <span
+            <span
+              className={css({
+                fontWeight: "bold"
+              })}
+            >
+              - Optimized Execution:
+            </span>{" "}
+            Breaks large swaps into smaller ones, helping you achieve better
+            overall pricing.
+          </li>
+          <li
             className={css({
-              fontWeight: "bold"
+              fontSize: ["md", "lg"],
+              marginBottom: "0.5rem",
+              color: "#fff" // Optional: Adjust text color for contrast
             })}
           >
-            - Mitigate Market Risk:
-          </span>{" "}
-          Achieve a better average entry price, especially in volatile or
-          down-trending markets.
-        </li>
-        <li
-          className={css({
-            fontSize: ["md", "lg"],
-            marginBottom: "0.5rem",
-            color: "#fff" // Optional: Adjust text color for contrast
-          })}
-        >
-          <span
+            <span
+              className={css({
+                fontWeight: "bold"
+              })}
+            >
+              - Mitigate Market Risk:
+            </span>{" "}
+            Achieve a better average entry price, especially in volatile or
+            down-trending markets.
+          </li>
+          <li
             className={css({
-              fontWeight: "bold"
+              fontSize: ["md", "lg"],
+              marginBottom: "0.5rem",
+              color: "#fff" // Optional: Adjust text color for contrast
             })}
           >
-            - Decentralized & Secure:
-          </span>{" "}
-          Powered by Bitcoin, non-custodial, and fully decentralized. Withdraw
-          your assets anytime with full control.
-        </li>
-      </ul>
+            <span
+              className={css({
+                fontWeight: "bold"
+              })}
+            >
+              - Decentralized & Secure:
+            </span>{" "}
+            Powered by Bitcoin, non-custodial, and fully decentralized. Withdraw
+            your assets anytime with full control.
+          </li>
+        </ul>
+      </div>
+      {/* { <ConnectWallet variant="createDcaButton" />} */}
     </VStack>
-  )
+  ) // todo improve connect wallet recipe to to return to home page if clicked
 }
 
 export default NoPositionsFound
